3c59ff581fa3c92b95c7cc50e157fc43d1e63e90
bijayrt <bijayrt@gmail.com>
2020-01-20 18:25:54 -0800

    Adding Publisher & repository object

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootStrap.java
@@ -0,0 +1,51 @@
+package guru.springframework.spring5webapp.bootstrap;
+/* BTuladhar created on 1/20/2020 */
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent> {
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
+
+    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData() {
+        Publisher worx = new Publisher("Workx");
+        publisherRepository.save(worx);
+
+        Publisher adw = new Publisher("Addison Wesley");
+        publisherRepository.save(adw);
+
+        Author eric = new Author("Eirc", "Event");
+        Book ddd = new Book("The domain driven design", "1234", adw);
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2ee Development without EJB", "1223", worx);
+        rod.getBooks().add(noEJB);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+}
---
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -0,0 +1,56 @@
+package guru.springframework.spring5webapp.model;
+/* BTuladhar created on 1/20/2020 */
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+
+    public Publisher() {
+    }
+
+    public Publisher(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
